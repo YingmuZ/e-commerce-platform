db95f0ea0b85cf756e29ad3c4ebe66e99c84c9ce
yingmu <wawamu2201@gmail.com>
2024-01-02 23:50:26 +0100

    change types and payload collection + modify graphql blocks/categories/products + generate schema + create Categories component (tbc)

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -20,6 +20,8 @@ import { generateMeta } from '../../_utilities/generateMeta'
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
 
+import Categories from '../../_components/Categories'
+
 import classes from './index.module.scss'
 
 export default async function Page({ params: { slug = 'home' } }) {
@@ -58,10 +60,13 @@ export default async function Page({ params: { slug = 'home' } }) {
 
   return (
     <React.Fragment>
-      {slug === 'hero' ? (
+      {slug === 'home' ? (
         <section>
           <Hero {...hero} />
-          <Gutter>{/* categories */}</Gutter>
+
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
+          </Gutter>
         </section>
       ) : (
         <>
---
+++ src/app/_components/Categories/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Category } from '../../../payload/payload-types'
+
+const Categories = ({ categories }: { categories: Category[] }) => {
+  return <div>Categories</div>
+}
+
+export default Categories
